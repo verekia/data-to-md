d64d88759f1459deb2c9182768a1ae30a08b1527
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2016-12-23 17:52:50 +0800

    h1 and table generation

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const fs = require('fs-extra')
+const yaml = require('js-yaml')
 
-const obj = fs.readJsonSync('data-test.json')
-
-obj.document.forEach((node) => {
-  if (node.type === 'table') {
-    console.log(node.content[0].join(' | '))
-  }
-})
+try {
+  const doc = yaml.safeLoad(fs.readFileSync('data-test.yaml', 'utf8'));
+  // console.log(doc);
+  let output = ''
+  doc.forEach((entry) => {
+    if (entry.hasOwnProperty('h1')) {
+      output += `\n# ${entry.h1}\n\n`
+    }
+    if (entry.hasOwnProperty('table')) {
+      output += entry.cols.reduce((a, b) =>
+        `${a} | ${b}`
+      )
+      output += '\n'
+      entry.cols.forEach((col, i, arr) => {
+        if (i !== 0 && i !== arr.length) {
+          output += ' | '
+        }
+        output += col.replace(/./g, '-')
+      })
+      output += entry.rows.reduce((a, b) =>
+        `${a}\n${b.reduce((c, d) =>
+          `${c} | ${d}`
+        )}`
+      , '')
+      output += '\n'
+    }
+  })
+  console.log(output)
+} catch (e) {
+  console.log(e)
+}
